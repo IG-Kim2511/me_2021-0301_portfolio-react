bf2bdfdbc3d55dfa37d2aeb0953c8d7db94ca301
iikim <iikim2511@gmail.com>
2021-03-01 17:36:49 -0500

    components ready

--- src/App.js
+++ src/App.js
@@ -5,13 +5,31 @@
  {/* components */}
  import './App.css';
 //  import { Button } from 'react-bootstrap';
+import Aboutme from './components/Aboutme'
+import Foot from './components/Foot'
+import Language from './components/Language'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+
 
  
 
 function App() {
   return (
     <div className="App">
- aaaaaaaaaaaaaaaa
+      <div>menu</div>
+        aaaaaaaaaaaaaaaa
+
+        <Aboutme/>
+        
+        <Projects></Projects>
+
+        <Language></Language>
+
+        <Skills></Skills>
+
+        <Foot></Foot>
+
     </div>
   );
 }
