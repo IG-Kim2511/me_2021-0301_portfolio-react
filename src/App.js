4049c86269dbcef53a052e4490d26a4d6d0833c6
iikim <iikim2511@gmail.com>
2021-03-01 22:36:45 -0500

    semantic ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
  /* eslint-disable */
 
  {/* library */}
+ import 'semantic-ui-css/semantic.min.css'
+ import { Icon } from 'semantic-ui-react'
 
  {/* components */}
  import './App.css';
-//  import { Button } from 'react-bootstrap';
+
+
 import Aboutme from './components/Aboutme'
 import Foot from './components/Foot'
 import Language from './components/Language'
@@ -12,13 +15,14 @@ import Projects from './components/Projects'
 import Skills from './components/Skills'
 
 
- 
-
 function App() {
   return (
     <div className="App">
       <div>menu</div>
         aaaaaaaaaaaaaaaa
+        <Icon disabled name='mail' />
+        <Icon disabled name='linkedin' />
+        <Icon disabled name='github' />
 
         <Aboutme/>
         
