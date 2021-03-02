3c863e28c1d51894ffc5819ca61000cd0a76f61a
iikim <iikim2511@gmail.com>
2021-03-02 13:48:07 -0500

    skills, js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,43 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Icon } from 'semantic-ui-react'
 
 
 {/* I'll make it with map loop */}
 
-const Skills = () => {
+
+
+function Skills() {
+    const [skill, setskill] = useState( [ 
+        "html5", "css3", "js", "react","sass", "git" 
+    ])
+   
     return (
         <div id="skills">
             <h1>skills</h1>
             <div className="about_skills">
                 <div>
                     <Icon disabled name='html5' className="skill_icon"/>
-                    <p>html</p>
-                </div>
-                
-                <div>
-                    <Icon disabled name='css3' />
-                    <p>css3</p>
+                    <p>{skill[0]}</p>
                 </div>
                 <div>
-                    <Icon disabled name='js' />
-                    <p>js</p>
-                </div>
-                
-                <div>
-                <Icon disabled name='react' />
-                    <p>react</p>
+                    <Icon disabled name='css3' className="skill_icon"/>
+                    <p>{skill[1]}</p>
                 </div>
                 <div>
-                <Icon disabled name='sass' />
-                    <p>sass</p>
-                </div>  
-                <div>
-                <Icon disabled name='git' />
-                    <p>git</p>
+                    <Icon disabled name='js' className="skill_icon"/>
+                    <p>{skill[2]}</p>
                 </div>
                 <div>
-                    and jQuery, Semantic-UI
+                    <Icon disabled name='react' className="skill_icon"/>
+                    <p>{skill[3]}</p>
                 </div>
+                <div>
+                    <Icon disabled name='sass' className="skill_icon"/>
+                    <p>{skill[4]}</p>
+                </div>
+                <div>
+                    <Icon disabled name='git' className="skill_icon"/>
+                    <p>{skill[5]}</p>
+                </div>              
             </div>
 
         </div>
---
+++ src/components/skills/Skills_data.js
@@ -0,0 +1,5 @@
+export const Skills_data =()=>{
+
+    name:'html5'
+
+}
